48b9f1a85289a283a4d854ac49f0ddb2ddbe4a9f
JonyVerma <94183777+JonyVerma@users.noreply.github.com>
2025-07-04 21:05:10 -0700

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,45 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n"
+
+$recipient = "jonyverma637@gmail.com"
+
+mail($recipient, $subject, $message, $mailheader)
+or die("Error!");
+
+echo'
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet" herf="style.css">
+
+</head>
+
+<body>
+    <div class="com">
+        <h1>Thank you for contacting me. i will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html"> Homepage</a>.</p>
+    </div>
+</body>
+
+</html>';
+
+?>
